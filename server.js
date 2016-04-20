06f4b87d2887244c394a7db12ed1e5574c3f56fd
Deni Chan <deni_chan_1990@yahoo.com>
2016-04-20 14:39:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     app = express();
 
-app.use(express.static('D:/WEB_DEV/training/AJAX/weatherReport'));
+app.use(express.static('__dirname'));
 app.listen(8080);
-console.log('server running... \n port : 8080 \n IP : 192.168.0.103')
+console.log('server running... \n port : 8080')
