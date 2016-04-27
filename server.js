98bbba0607ffb34a053fa8a4d5bb7fa0ea3e29ef
Deni Chan <deni_chan_1990@yahoo.com>
2016-04-28 00:07:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     app = express();
 
-app.use(express.static('__dirname'));
+app.use(express.static(__dirname));
 app.listen(8080);
 console.log('server running... \n port : 8080')
